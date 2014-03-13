8144bf8f91c8360f95f1db0e34040d11360b4b8e
Jalen Lyu <jalen.lyu@gmail.com>
2014-03-13 15:00:18 +0800

    init ws communication tunnel

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express')
+  , app = express()
+  , http = require('http')
+  , server = http.createServer(app)
+  , io = require('socket.io').listen(server);
+
+app.use(express.static(__dirname + '/public'));
+
+server.listen(3000);
+
+var peers = {};
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('welcome', { hello: 'world' });
+  socket.on('join', function (data) {
+    var username = data.username;
+    peers[username] = socket;
+    socket.on('message', function (data) {
+      var peer = peers[data.to];
+      if (!peer) return;
+
+      peer.emit('message', {
+        from: username,
+        data: data.data,
+      });
+    });
+  });
+});
